fc88e75f2cb077ab8219dc5fc5496ef1283af50b
John Salvatòre <nullstephania@gmail.com>
2018-10-28 21:18:22 +0300

    Reworked shot register

--- History v2.lua
+++ History v2.lua
@@ -18,19 +18,6 @@ local Elements = {
     end)
 }
 
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
-
 local function TicksTime(tick)
     return globals.tickinterval() * tick
 end
