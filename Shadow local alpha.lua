11f96671f4b5919024231af2250ff0badf75fc3f
John Salvatòre <ptickcount@gmail.com>
2019-03-23 15:36:07 +0300

    Update Shadow local alpha.lua

--- Shadow local alpha.lua
+++ Shadow local alpha.lua
@@ -5,55 +5,11 @@ local menu = {
     active = ui.new_checkbox("Visuals", "Colored models", "Shadow pulsating"),
     max_alpha = ui.new_slider("Visuals", "Colored models", "Maximum shadow alpha", 25, 255, 175, true),
     threshold = ui.new_slider("Visuals", "Colored models", "Alpha threshold", 0, 100, 25, false),
+    speed = ui.new_slider("Visuals", "Colored models", "Alpha speed", 0, 100, 90, true, "%"),
 }
 
 local act = 1
 local alpha = 0
-
-local fps_prev = 0
-local last_update_time = 0
-local frametimes = { }
-
-local function get_fps()
-    local rt, ft = globals.realtime(), globals.absoluteframetime()
-
-    if ft > 0 then 
-        table.insert(frametimes, 1, ft)
-    end
-
-    local count = #frametimes
-    if count == 0 then
-        return 0
-    end
-
-    local accum = 0
-    local i = 0
-    while accum < 0.5 do
-        i = i + 1
-        accum = accum + frametimes[i]
-        if i >= count then
-            break
-        end
-    end
-    
-    accum = accum / i
-    
-    while i < count do
-        i = i + 1
-        table.remove(frametimes)
-    end
-    
-    local fps = 1 / accum
-    local time_since_update = rt - last_update_time
-    if math.abs(fps - fps_prev) > 4 or time_since_update > 1 then
-        fps_prev = fps
-        last_update_time = rt
-    else
-        fps = fps_prev
-    end
-    
-    return math.floor(fps + 0.5)
-end
 
 client.set_event_callback("paint", function(c)
 
@@ -65,10 +21,7 @@ client.set_event_callback("paint", function(c)
     local max_alpha = ui.get(menu.max_alpha)
     local threshold = ui.get(menu.threshold)
 
-    local cur_fps = get_fps()
-    local max_fps = cur_fps > 300 and cur_fps or 300
-    
-    local factor = max_fps / cur_fps
+    local factor = 255 / (ui.get(menu.speed) / 100) * globals.frametime()
 
     if alpha > max_alpha + threshold then act = -factor end
     if alpha < -threshold then act = factor end
