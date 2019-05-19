e38050aea4c2ddba252a20cb95a02e804013ead0
Danil Salvatore <35622087+tickcount@users.noreply.github.com>
2019-05-19 12:15:12 +0300

    Update Override min damage.lua

--- Override min damage.lua
+++ Override min damage.lua
@@ -1,34 +1,48 @@
-local var_cached = nil
-local min_dmg_table = { [0] = "Auto" }
+local set_dmg_list = function()
+    local damage_list = { }
 
-for i = 1, 26 do
-    -- HP + {1-26}
-    min_dmg_table[100 + i] = "HP+" .. i
+    damage_list[0] = "Auto"
+
+    for i = 1, 26 do
+        -- HP + {1-26}
+        -- HP = 0 -> Auto
+    
+        damage_list[100 + i] = "HP+" .. i
+    end
+
+    return damage_list
 end
 
-local minimum_damage = ui.reference("RAGE", "Aimbot", "Minimum damage")
+local data = {
+    var_cached = nil,
+    minimum_damage = ui.reference("RAGE", "Aimbot", "Minimum damage"),
 
-local is_active = ui.new_checkbox("RAGE", "Other", "Override minimum damage")
-local override_key = ui.new_hotkey("RAGE", "Other", "\n damage_override_key", true)
-local damage_value = ui.new_slider("RAGE", "Other", "\n damage_override_value", 0, 126, 55, true, "", 1, min_dmg_table)
+    is_active = ui.new_checkbox("RAGE", "Other", "Override minimum damage"),
+    override_key = ui.new_hotkey("RAGE", "Other", "\n damage_override_key", true),
+    damage_value = ui.new_slider("RAGE", "Other", "\n damage_override_value", 0, 126, 55, true, "", 1, set_dmg_list())
+}
 
-local setup_visible = function(c)
-    ui.set_visible(damage_value, ui.get(c))
+data.set_visible = function(_self)
+    if not _self then
+        ui.set_callback(data.is_active, data.set_visible)
+    end
+
+    ui.set_visible(data.damage_value, ui.get(data.is_active))
 end
 
-client.set_event_callback("paint", function(c)
-    var_cached = var_cached ~= nil and var_cached or ui.get(minimum_damage)
+data.callback = function()
+    data.var_cached = data.var_cached ~= nil and data.var_cached or ui.get(data.minimum_damage)
 
-    if ui.get(is_active) and ui.get(override_key) then
-        ui.set(minimum_damage, ui.get(damage_value))
+    if ui.get(data.is_active) and ui.get(data.override_key) then
+        ui.set(data.minimum_damage, ui.get(data.damage_value))
         renderer.indicator(255, 255, 255, 150, "DMG")
     else
-        if var_cached ~= nil then
-            ui.set(minimum_damage, var_cached)
-            var_cached = nil
+        if data.var_cached ~= nil then
+            ui.set(data.minimum_damage, data.var_cached)
+            data.var_cached = nil
         end
     end
-end)
+end
 
-ui.set_callback(is_active, setup_visible)
-setup_visible(is_active)
+data.set_visible()
+client.set_event_callback("paint", data.callback)
