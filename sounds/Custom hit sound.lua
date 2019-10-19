37acf878052454853df021651ce444973981d560
Danil Salvatore <35622087+tickcount@users.noreply.github.com>
2019-10-19 18:52:03 +0300

    Update Custom hit sound.lua

--- sounds/Custom hit sound.lua
+++ sounds/Custom hit sound.lua
@@ -21,6 +21,7 @@ local sounds = {
     ["Hentai 3"] = "hentai3.mp3",
     ["osu! combobreak"] = "combobreak.wav",
     ["PUBG Pan"] = "pubg_pan.mp3",
+    ["uwu daddy ft. kano"] = "kano.mp3",
 }
 
 local sound_names = {}
@@ -34,12 +35,12 @@ end
 table.sort(sound_names)
 
 if #sound_names <= 0 then
-    client.log('hitmarker.lua > there is no sounds "\\csgo\\sound"')
-    return _exception()
+    return error('hitmarker.lua > there is no sounds "\\csgo\\sound"')
 end
 
 local enabled_ref = ui.new_checkbox("VISUALS", "Player ESP", "Override hit marker sound")
 local sound_ref = ui.new_combobox("VISUALS", "Player ESP", "\n hitsound_override", sound_names)
+local sound_vol = ui.new_slider("VISUALS", "Player ESP", "Volume multiplier \n hitsound_vol_multiplier", 1, 50, 10, true, "x", 0.1)
 
 client.set_event_callback("player_hurt", function(e)
     if not ui_get(enabled_ref) or e.attacker == nil then 
@@ -51,11 +52,29 @@ client.set_event_callback("player_hurt", function(e)
     local userid = client.userid_to_entindex(e.userid)
     local attacker = client.userid_to_entindex(e.attacker)
 
-    for k,v in pairs(sounds) do
-        if k == ui_get(sound_ref) and sound_exists(v) and attacker == me and userid ~= me then
-            local time = globals.tickinterval() * 1
-            
-            client.delay_call(time, client.exec, "play " .. v)
+    local vol = ui.get(sound_vol) / 10
+
+    for k, v in pairs(sounds) do
+        if attacker == me and userid ~= me and k == ui_get(sound_ref) and sound_exists(v) then
+            local command = "playvol " .. v .. " "
+
+            if vol <= 1 then
+                client.exec(command .. vol)
+            else
+                local m_vol = math.floor(vol + 0.9)
+
+                for i=1, m_vol do
+                    local volume = 1
+
+                    if m_vol ~= vol and i==m_vol then
+                        volume = vol-math.floor(vol)
+                    end
+
+                    client.exec(command .. volume)
+                end
+            end
+
+            return
         end
     end
 end)
